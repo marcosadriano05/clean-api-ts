9ce59eff533c9be08893168007213c69b966a70c
Marcos Adriano <marcosadriano740@gmail.com>
2021-09-22 20:37:39 -0300

    fix: ensure CompareFieldsValidation returns a InvalidParamError with the fieldToCompare

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -11,7 +11,7 @@ export class CompareFieldsValidation implements Validation {
 
   validate (input: any): Error {
     if (input[this.fieldName] !== input[this.fieldNameToCompare]) {
-      return new InvalidParamError(this.fieldName)
+      return new InvalidParamError(this.fieldNameToCompare)
     }
   }
 }
