a437399895b61b6ca35a8c0ca17bf69667757a20
Marcos Adriano <marcosadriano740@gmail.com>
2021-09-24 13:29:59 -0300

    refactor: remove redundant code

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -10,6 +10,6 @@ export class JwtAdapter implements Encrypter {
   async encrypt (value: string): Promise<string> {
     const payload = { id: value }
     const accessToken = jwt.sign(payload, this.secret)
-    return await new Promise(resolve => resolve(accessToken))
+    return accessToken
   }
 }
