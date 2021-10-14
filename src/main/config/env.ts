7c5a53c596299623cdfbfbe1f4ce2ff01cf6678d
Marcos Adriano <marcosadriano740@gmail.com>
2021-10-14 20:01:40 -0300

    chore: add docker compose with mongo image

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-api-ts',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-api-ts',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'sUf$,_as'
 }
