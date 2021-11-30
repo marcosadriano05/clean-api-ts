698d88c7ca302493e3ee02254cd5f77a8b7c9f3a
Marcos Adriano <marcosadriano740@gmail.com>
2021-11-30 12:35:07 -0300

    feat: ensure AddAccount only add if email is not persisted before

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,14 +1,19 @@
-import { AccountModel, AddAccount, AddAccountModel, AddAccountRepository, Hasher } from './db-add-account-protocols'
+import { AccountModel, AddAccount, AddAccountModel, AddAccountRepository, Hasher, LoadUserByEmailRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadUserByEmailRepository: LoadUserByEmailRepository
   ) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const hashedPassword = await this.hasher.hash(accountData.password)
-    const account = await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
-    return account
+    const existentAccount = await this.loadUserByEmailRepository.loadByEmail(accountData.email)
+    if (!existentAccount) {
+      const hashedPassword = await this.hasher.hash(accountData.password)
+      const account = await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
+      return account
+    }
+    return null
   }
 }
